f782fde6001a4dc7f009d897137687fea4f96ca4
01Que <224067780@tut4life.ac.za>
2023-04-18 10:01:21 -0700

    second-commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,38 +9,43 @@ const showMenu = (toggleId, navId) =>{
         })
     }
 }
+
+function sendEmail() {
+    // Get the text from the textarea
+    const emailText = document.getElementById("email-text").value;
+  
+    // Encode the email body as a URL-encoded string
+    const body = encodeURIComponent(emailText);
+  
+    // Set the email address and subject
+    const emailAddress = "buelaniquinton86@gmail.com";
+    const subject = "Email from my website";
+  
+    // Construct the mailto: URL
+    const mailtoUrl = `mailto:${emailAddress}?subject=${subject}&body=${body}`;
+  
+    // Open the user's default email client with the prepopulated email
+    window.location.href = mailtoUrl;
+}
+  
 showMenu('nav-toggle','nav-menu')
+/*changing themes*/
+const toggle = document.getElementById('toggleDark');
+const body = document.querySelector('body');
 
-/*==================== REMOVE MENU MOBILE ====================*/
-const navLink = document.querySelectorAll('.nav__link')
-
-function linkAction(){
-    const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction))
-
-/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
-const sections = document.querySelectorAll('section[id]')
-
-function scrollActive(){
-    const scrollY = window.pageYOffset
-
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute('id')
-
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
-        }else{
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
-        }
-    })
-}
-window.addEventListener('scroll', scrollActive)
-
+toggle.addEventListener('click', function(){
+    this.classList.toggle('bi-moon');
+    if(this.classList.toggle('bi-brightness-high-fill')){
+        body.style.background = 'white';
+        body.style.color = 'black';
+        body.style.transition = '2s';
+    }else{
+        body.style.background = 'black';
+        body.style.color = 'white';
+        body.style.transition = '2s';
+    }
+});
+/*changing themes*/
 /*===== SCROLL REVEAL ANIMATION =====*/
 const sr = ScrollReveal({
     origin: 'top',
